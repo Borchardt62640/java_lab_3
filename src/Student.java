623a94c6803ef84bc401bfde3d054b1c27355ddb
Borchardt62640 <piotr3borchardt@gmial.com>
2022-01-15 19:41:30 +0100

    cw1

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Student {
+    String str1;
+    String str2;
+    int int1;
+    Boolean bol1;
+}
+
+
